de0575ce1828319053280cdeb081ec5a6d71a9e5
Victor Emanuel dos Santos Oliveira <VictorEmanuell>
2021-06-04 21:41:51 -0300

    points_ok_1

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 const axios = require('axios');
 require('dotenv').config();
 
+const points_ok = require('./points_ok.json')
+
 const config = {
     prefix: process.env.DS_PREFIX,
     token: process.env.DS_TOKEN,
@@ -53,6 +55,19 @@ client.on('message', async message => {
                         if (response.result === 0) {
                             (await message.author.dmChannel.send("```Talvez você tenha digitado seu ID/Username errado, não te encontrei.\nDigite !pontos para tentar novamente.```")).delete({ timeout: 10000 })
                         } else {
+                            let points = new Map()
+
+                            for(let object of points_ok){
+                                points.set(object.id, object)
+                            }
+
+                            let status
+
+                            if(points.has(response.data[0].user_id)){
+                                status = 'Status da transferência: ✅  **Pontos transferidos!**'
+                            } else {
+                                status = 'Status da transferência: 🟦 **Aguarde, seus pontos seram transferidos em até 5 dias úteis**'
+                            }
 
                             let watchtime_number = Number(response.data[0].watchtime)
                             let watchtime = (watchtime_number / 60).toFixed(0)
@@ -61,7 +76,7 @@ client.on('message', async message => {
                                 .setColor('#FEB34D')
                                 .setAuthor(message.author.tag)
                                 .setTitle(`Você tem ${response.data[0].points} VELLOSOpoints`)
-                                .setDescription(`Username: **${response.data[0].user}**\nID: **${response.data[0].user_id}**\nWatchTime: **${watchtime}** horas\n\n**OBS.:** É importante que você digite seu Username corretamente, sem ignorar letras maiúsculas/minúsculas, e os espaços! Caso esteja em dúvida sobre os pontos, confira se seu ID BooYah corresponde com o apresentado nesta mensagem.\n\n**- Você também pode ver seus VellosoPoints digitando !points no chat da live ;)**`)
+                                .setDescription(`Username: **${response.data[0].user}**\nID: **${response.data[0].user_id}**\nWatchTime: **${watchtime}** horas\n\n${status}\n\n**OBS.:** É importante que você digite seu Username corretamente, sem ignorar letras maiúsculas/minúsculas, e os espaços! Caso esteja em dúvida sobre os pontos, confira se seu ID BooYah corresponde com o apresentado nesta mensagem.\n\n**- Você também pode ver seus VellosoPoints digitando !points no chat da live ;)**`)
                                 .setThumbnail('https://cdn.discordapp.com/icons/515558123998674944/9ad65964373770f0c86299f8ff4c600d.png?size=256', { dynamic: true, format: "png", size: 1024 })
                                 .setTimestamp();
 
